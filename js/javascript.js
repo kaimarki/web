8588ce874da3e6095f8454912b3cd91a922c6e8d
kaimarki <104058991+kaimarki@users.noreply.github.com>
2022-04-20 11:09:04 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -15,17 +15,3 @@ polygons.addTo(map)
 }
 addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')  //github??
 
-// add popup to each feature
-function popUPinfo(feature, layer) {
-layer.bindPopup(feature.properties.NIMI)
-}
-// add geoJSON polygons layer
-async function addDistrictsGeoJson(url) {
-const response = await fetch(url)
-const data = await response.json()
-const polygons = L.geoJson(data, {
-onEachFeature: popUPinfo,
-})
-polygons.addTo(map)
-}
-addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
