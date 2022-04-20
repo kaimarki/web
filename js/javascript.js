237f38c33cc1deff5517059821eaa98df659bc18
kaimarki <104058991+kaimarki@users.noreply.github.com>
2022-04-20 10:28:33 +0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,31 @@
+let map = L.map('map').setView([58.373523, 26.716045], 12)
+const osm =
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+maxZoom: 19,
+attribution: 'OpenStreetMap contributors',
+})
+osm.addTo(map)
+
+// add geoJSON polygons layer*
+async function addDistrictsGeoJson(url) {
+const response = await fetch(url)
+const data = await response.json()
+const polygons = L.geoJson(data)
+polygons.addTo(map)
+}
+addDistrictsGeoJson('andmed/tartu_city_districts_edu.geojson')  //github??
+
+// add popup to each feature
+function popUPinfo(feature, layer) {
+layer.bindPopup(feature.properties.NIMI)
+}
+// add geoJSON polygons layer
+async function addDistrictsGeoJson(url) {
+const response = await fetch(url)
+const data = await response.json()
+const polygons = L.geoJson(data, {
+onEachFeature: popUPinfo,
+})
+polygons.addTo(map)
+}
+addDistrictsGeoJson('andmed/tartu_city_districts_edu.geojson')
