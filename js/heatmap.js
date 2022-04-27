3b0e3f3b5b9e78f5deadaecc8adbf4be31ef2cb2
kaimarki <104058991+kaimarki@users.noreply.github.com>
2022-04-27 10:43:04 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -18,5 +18,13 @@ addGeoJson('geojson/tartu_city_celltowers_edu.geojson')
 async function addGeoJson(url) {
 const response = await fetch(url)
 const data = await response.json()
-console.log(data.features[0])
+const heatData = data.features.map(heatDataConvert)
+console.log(heatData)
+}
+function heatDataConvert(feature) {
+return [
+feature.geometry.coordinates[1],
+feature.geometry.coordinates[0],
+feature.properties.area,
+]
 }
