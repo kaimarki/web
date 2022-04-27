5bd26151f1f7603225a8d6f478aabf85e3dec245
kaimarki <104058991+kaimarki@users.noreply.github.com>
2022-04-27 11:41:53 +0300

    Update choroplethmap.js

--- js/choroplethmap.js
+++ js/choroplethmap.js
@@ -1,14 +1,14 @@
 let map = L.map('map').setView([58.373523, 26.716045], 12)
 const osm =
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-maxZoom: 19,
-attribution: 'OpenStreetMap contributors',
-})
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: 'OpenStreetMap contributors',
+    })
 osm.addTo(map)
 
 // default map settings
 function defaultMapSettings() {
-map.setView([58.373523, 26.716045], 12)
+    map.setView([58.373523, 26.716045], 12)
 }
 
 addGeoJson('geojson/tartu_city_districts_edu.geojson')
@@ -16,21 +16,21 @@ addGeoJson('geojson/tartu_city_districts_edu.geojson')
 
 // add geoJSON layer
 async function addGeoJson(url) {
-const response = await fetch(url)
-const data = await response.json()
-L.choropleth(data, {
-valueProperty: 'OBJECTID',
-scale: chroma.scale(['#fafa6e','#2A4858'])
-    .mode('lch').colors(5),
-steps: 5,
-mode: 'q', // q for quantile, e for equidistant
-style: {
-color: '#fff',
-weight: 2,
-fillOpacity: 0.7,
-},
-onEachFeature: function (feature, layer) {
-layer.bindPopup('Towers: ' + feature.properties.TOWERS)
-},
-}).addTo(map)
+    const response = await fetch(url)
+    const data = await response.json()
+    L.choropleth(data, {
+        valueProperty: 'OBJECTID',
+        scale: chroma.scale(['#fafa6e', '#2A4858'])
+            .mode('lch').colors(5),
+        steps: 5,
+        mode: 'q', // q for quantile, e for equidistant
+        style: {
+            color: '#fff',
+            weight: 2,
+            fillOpacity: 0.7,
+        },
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup('Towers: ' + feature.properties.TOWERS)
+        },
+    }).addTo(map)
 }
